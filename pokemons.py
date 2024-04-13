955e86c70d731e0319cf316e23357c708ac99c64
marcel araujo <marcelaraujo1440@gmail.com>
2024-04-13 11:31:54 -0300

    Update pokemons.py

--- pokemons.py
+++ pokemons.py
@@ -1,20 +1,21 @@
-while True:
-    import random
+import random
 
-    pokemons_caverna=['Aggron','Larvitar','Aron','Pupitar']
-    pokemons_mato=['bulbasaur','Pansage','Turtwig','Treecko']
-    pokedex=[]
+pokemons_caverna=['Aggron','Larvitar','Aron','Pupitar']
+pokemons_mato=['bulbasaur','Pansage','Turtwig','Treecko']
+pokedex=[]
+
     
-    print(f"*professor carvalho aparece*")
-    nomeJogador = input(f"Professor Carvalho - olá treinador, bem vindo ao mundo Pokemón. Eu sou o professor Carvalho, como posso te chamar? \nDigite aqui seu nome: ")
-    print(f"Professor carvalho - É um prazer te conhecer, {nomeJogador}. Esta é a cidade de Pallet, da região de Kanto!\n")
-
+print(f"*professor carvalho aparece*")
+nomeJogador = input(f"Professor Carvalho - olá treinador, bem vindo ao mundo Pokemón. Eu sou o professor Carvalho, como posso te chamar? \nDigite aqui seu nome: ")
+print(f"Professor carvalho - É um prazer te conhecer, {nomeJogador}. Esta é a cidade de Pallet, da região de Kanto!\n")
+    
     #escolha do bioma
-    
+for i in pokemons_mato:  
     escolha=int(input("Escolha para onde deseja ir\n1. Ir para o Mato\n2. Ir para a Caverna\n3. Abrir Pokedex\n4. Sair\n:"))
     if escolha >4 or escolha<0:
         print("\nNão foi possível encontrar alguma função com este número digitado!\n")
-        continue
+    
+
     if escolha == 1:
         
         pok=random.choice(pokemons_mato)
@@ -22,6 +23,7 @@ while True:
         captura=input("Você deseja capturar este pokemon? (s/n): \n")
         if captura == 's':
             print(f"Você capturou o {pok}\n")
+            pokedex.append(i)
                
         elif captura == 'n':
             print(f"Ok, você escolheu não capturar o {pok}.")  
