4dbacbe76cbd8abecafe08b4adb484e550005156
marcel araujo <marcelaraujo1440@gmail.com>
2024-04-21 13:14:15 -0300

    s

--- pokemons.py
+++ pokemons.py
@@ -5,85 +5,95 @@ import random
 pokemonsIniciais = ["Bulbasaur","Squirtle","Charmander"]
 pokemons_caverna=['Onix','Zubat','Diglet','Geodude']
 pokemons_mato=['Bulbasaur','Caterpie','Bellsprout','Abra']
+rep_floresta=[]
+rep_caverna=[]
 pokedex=[]
 tentativas = 3
     
 #introdução professor carvalho, e escolha de um nome para o jogador
 print(f"professor carvalho aparece")
 nomeJogador = input(f"Professor Carvalho - olá treinador, bem vindo ao mundo Pokemón. Eu sou o professor Carvalho, como posso te chamar? \nDigite aqui seu nome: ")
-print(f"Professor carvalho - É um prazer te conhecer, {nomeJogador}. Esta é a cidade de Pallet, da região de Kanto!\n")
+print(f"\nProfessor carvalho - É um prazer te conhecer, {nomeJogador}. Esta é a cidade de Pallet, da região de Kanto!\n")
 
 #escolhendo um pokemon inicial
-pokeInicial = int(input(f"Você pode escolher um pokemon inicial para ser seu primeiro parceiro, escolha um\n1. Bulbasaur\n2. Squirtle\n3. Charmander\n"))
+pokeInicial = int(input(f"Você pode escolher um pokemon inicial para ser seu primeiro parceiro, escolha um\n(1) Bulbasaur\n(2) Squirtle\n(3) Charmander\n"))
 if pokeInicial == 1:
      print(f"{pokemonsIniciais[0]}! um tipo planta, otima escolha!\n")
      pokedex.append(pokemonsIniciais[0])
+     rep_floresta.append(pokemonsIniciais[0])
 if pokeInicial == 2:
      print(f"{pokemonsIniciais[1]}! um tipo água, otima escolha!\n")
      pokedex.append(pokemonsIniciais[1])
 if pokeInicial == 3:
      print(f"{pokemonsIniciais[2]}! um tipo fogo, otima escolha!\n")
      pokedex.append(pokemonsIniciais[2])
-if tentativas == 0: #nao esta funcionando 
-        print("Você atingiu o limite de tentativas extras!")
-        
+    
     #escolha do bioma
-while True:
-    escolha=int(input("Escolha para onde deseja ir\n1. Ir para a Floresta\n2. Ir para a Caverna\n3. Abrir Pokedex\n4. Sair\n:"))
+
     
     #checkando validade de escolha
-    if escolha >4 or escolha<0:
-        print("\nNão foi possível encontrar alguma função com este número digitado!\n")
+   
+escolhas= True
+#criando  a escolha do bioma da Floresta
+while escolhas == True:
+    escolha = int(input(f'\nOque deseja fazer?\n(1) Ir para a Floresta\n(2) Ir para a Caverna\n(3) Pokedex\n() Sair \n>>> '))
+    
         
-    #criando  a escolha do bioma da Floresta
-    if escolha == 1:
+    if escolha == 1 :
         
-        #escolha randomica do pokemon que o jogador vai encontrar na floresta!
-        pok = random.choice(pokemons_mato)
-        print(f"Você entrou na Floresta e encontou um {pok}\n")
-        #escolha do jogador se ele vai ou nao querer capturar o pokemon
-        captura = input("Você deseja capturar este pokemon? (s/n): \n")
+        pok = random.randint(0, pokemons_mato.index("Abra"))
         
-        #checagem de escolha
-        if captura != "s" and captura != "n":
-            print('Tente novamente com uma opção válida')
+        if pok in rep_floresta:
+            print(f'\nVocê encontrou um {pokemons_mato[pok]}, mas você ja possui um!')
+            continue
+        print(f'\n{pokemons_mato[pok]} apareceu\n')
         
-        #caso o jogador escolha capturar: 
-        if captura == 's':
-             #sorteio se o jogador ira ou não capturar com uma probabilidade de captura de 50%
-                num1 = random.randint(0,1)
-                num2 = random.randint(0,1)
+        processo = True
+        while processo == True:
+            captura = input(f'Você deseja capturar este pokemon? (s/n)')
+    
                 
-                if num1 == num2:
-                    print(f"Você capturou o {pok}\n") #caso haja a captura
-                    pokedex.append(pok)
-                    continue
+            if captura == 's':
                 
-                while tentativas >0: #problema aq talvez nao seja while
-                    
-                    if num1 != num2:
-                        novam = input('Voce nao consegui capturar o pokemon!\nDeseja tentar capturar novamente? (s/n) ') #caso não haja captura e o usuarrio recebe novas opções
-                        tentativas-=1
-                       
-                        if novam != 's':
-                                print(f'Voce não capturou o {pok}!\n' ) #caso haja captura na segunda tentativa
-                    
+                    num1=random.randint(0,1)
+                    num2=random.randint(0,1)
+                    if num1==num2: 
+                        print("Você capturou esse pokemon!")
+                        pokedex.append(pokemons_mato[pok])
+                        rep_floresta.append(pok)
+                        break
+                    while tentativas > 0:
                         
+                        if num1!=num2:
+                            novamente=input("você nao conseguiu capturar esse pokemon, deseja tentar novamente? (s/n)")
+                            if novamente == 's':
+                                tentativas-=1
+                                num1=random.randint(0,1)
+                                num2=random.randint(0,1)
+                                if num1==num2:
+                                    print("Você capturou esse pokemon!")
+                                    pokedex.append(pokemons_mato[pok])
+                                    rep_floresta.append(pok)
+                                    break
+                                elif num1 != num2:
+                                    print("O pokemon fugiu!")
+                                    break
+                            if novamente == 'n':
+                                continue
+                    if tentativas == 0:
+                        print("\nVocê nao conseguiu capturar este pokemon!")
+            if captura == 'n' :
+                print(f'\nOk você escolheu não capturar,{pokemons_mato[pok]} ')
+                break 
+            else:
+                print("Escolha uma opção válida!")
+            processo = False
+            break
+                
                         
-                    
-                    
-                    if novam == 's':
-                                print(f"Você capturou o {pok}\n") 
-                                pokedex.append(pok)
-                                
-                                print(f"você tem: {tentativas} tentativas")
-                                
-                    elif novam == 'n':
-                            print(f"Ok, você escolheu não capturar o {pok}.\n")  
-                    
-                    elif captura == 'n':
-                        print(f"Ok, você escolheu não capturar o {pok}.")  
-            
+        
+        continue
+    
     #caso a escolha seja a da carverna
     if escolha == 2:
         
@@ -101,8 +111,8 @@ while True:
         
         #caso o jogador queira tentar capturar
         if captura == 's':
-             
-                #sorteando a chance de 50% da captura do pokemon
+            
+                #sorteando a chance de 30% da captura do pokemon
                 num1= random.randint(0,3)
                 num2= random.randint(0,3)
         
@@ -138,13 +148,14 @@ while True:
                         print(f"Ok, você escolheu não capturar o {pok}.")
         
     if escolha == 3:
-        print(f"aqui são todos os pokemons que você ja capturou {pokedex}") 
+        if len(pokedex) <1 :
+            print("Você ainda não capturou nenhum pokemon! ")
+        print(f"Aqui são todos os pokemons que você ja capturou {pokedex}") 
     
-    if tentativas == 0 or novamente == ' s':
-        print("Você atingiu o limite de tentativas extras!")
-            
-            
+    if tentativas == 0: #nao esta funcionando 
+            print("Você atingiu o limite de tentativas extras!")
+            continue
     
     if escolha == 4:
-            print("Até logo!")
-            break
+        print("Até logo!")
+        break
